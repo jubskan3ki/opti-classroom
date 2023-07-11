52791bf68b4f62b68856a9fbd0bdd82803c4ffac
jubskan3ki <93186899+jubskan3ki@users.noreply.github.com>
2023-07-11 17:02:35 +0200

    Push

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,48 @@
 import React from 'react'
-
+import styles from './Home.module.css';
 export default function home() {
-  return (
-    <div>home</div>
-  )
+    return (
+        <div className={styles.Content}>
+            <div className={styles.ContentLeft}>
+                <div className={styles.Card}>
+
+                </div>
+                    
+                <div className={styles.DivRoom}>
+                    <div className={styles.SelectRoom}>
+                        <select name="Room" >
+                            <option value="Room1">Room1</option>
+                            <option value="Room2">Room2</option>
+                            <option value="Room3">Room3</option>
+                        </select>
+                    </div>
+                    <div className={styles.DivRoomLeft}>
+                        <div className={styles.Card}>
+
+                        </div>
+                        <div className={styles.Card}>
+
+                        </div>
+                        <div className={styles.Card}>
+
+                        </div>
+                        <div className={styles.Card}>
+
+                        </div>
+                    </div>
+                    <div className={styles.Card}>
+
+                    </div>
+                </div>
+                    
+                <div className={styles.Card}>
+
+                </div>
+            </div>
+            <div className={styles.ContentRight}>
+
+            </div>
+
+        </div>
+    )
 }
