2548cc993f4ff48c95deeb91ec2cb5d4140b93bd
Hugo <hgranier20@gmail.com>
2023-07-11 23:53:21 +0200

    premier test push , composant card

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,19 @@
 import Head from 'next/head'
-import styles from '@/styles/Home.module.css'
+import styles from './Login.module.css'
 
 
 export default function Home() {
   return (
       <main >
         
+        <div className={styles.Content}>
+          <div className={styles.ContentLeft}>
+            <div className={styles.CardLeft}>
+              <h1 className={styles.Title}>Se connecter</h1>
+              </div>
+          </div>
+          <div className={styles.ContentRight}><div className={styles.CardRight}>test</div></div>
+        </div>
       </main>
   )
 }
