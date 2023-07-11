92851f63b2ee9df58f2aaa96cb780eb54874d86d
jubskan3ki <93186899+jubskan3ki@users.noreply.github.com>
2023-07-11 12:07:26 +0200

    push

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Head from 'next/head'
 
 export default function Error404() {
-  return (
-    <div>404</div>
-  )
+    return (
+        <>
+
+            <div>404</div>
+
+        </>
+    )
 }
