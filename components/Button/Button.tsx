45c04914353c2d8fb8596a2cd833712c927ba259
Hugo <hgranier20@gmail.com>
2023-07-15 18:48:46 +0200

    wip : plannig page begin

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -4,11 +4,14 @@ import styles from './Button.module.css';
 interface ButtonProps {
   text: string;
   onClick: (event: MouseEvent<HTMLButtonElement>) => void;
+  variant?: string;
 }
 
-const Button = ({ text, onClick }: ButtonProps) => {
+const Button = ({ text, onClick, variant }: ButtonProps) => {
+  const buttonClassName = variant ? `${styles.Button} ${styles.Button2}` : styles.Button;
+
   return (
-    <button onClick={onClick} className={styles.Button}>
+    <button onClick={onClick} className={buttonClassName}>
       {text}
     </button>
   );
--- src/pages/Planning/index.tsx
+++ src/pages/Planning/index.tsx
@@ -1,7 +1,80 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Select from '../../../components/Select/Select'
+import Button from '../../../components/Button/Button';
+import styles from './Planning.module.css'
 
 export default function Planning() {
+
+  const [selectedValue, setSelectedValue] = useState('');
+  const handleSelectChange = (id: string) => {
+    setSelectedValue(id);
+  };
+  const [selectsRoom, setSelectsRoom] = useState([
+    {
+        value: 'Salon',
+        label: 'Salon',
+        id: 'id1'
+    },
+    {
+        value: 'Chambre',
+        label: 'Chambre',
+        id: 'id2'
+    },
+    {
+        value: 'Cuisine',
+        label: 'Cuisine',
+        id: 'id3'
+    },
+    {
+        value: 'Garage',
+        label: 'Garage',
+        id: 'id4'
+    },
+    {
+      value: 'Salle de bain',
+      label: 'Salle de bain',
+      id: 'id5'
+  }
+  ]);
+  const [selectsWeek, setSelectsWeek] = useState([
+  {
+      value: 'Semaine 1',
+      label: 'Semaine 1',
+      id: 'id1'
+  },
+  {
+      value: 'Semaine 2',
+      label: 'Semaine 2',
+      id: 'id2'
+  },
+  {
+      value: 'Semaine 3',
+      label: 'Semaine 3',
+      id: 'id3'
+  },
+  {
+      value: 'Semaine 4',
+      label: 'Semaine 4',
+      id: 'id4'
+  }
+  ]);
   return (
-    <div>Planning</div>
+    <div>
+      <div className={styles.ContentHigh}>
+        <Select 
+          id="01" 
+          options={selectsRoom} 
+          selectedValue={selectedValue}
+          onChange={handleSelectChange}    
+        />
+        <Select 
+          id="01" 
+          options={selectsWeek} 
+          selectedValue={selectedValue}
+          onChange={handleSelectChange}    
+        />
+        <Button text='Setting Room' variant='primary'/>
+      </div>
+    </div>
   )
 }
