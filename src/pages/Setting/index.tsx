281ae5230bff346120caddacd91a0e2dee9b48fd
jubskan3ki <93186899+jubskan3ki@users.noreply.github.com>
2023-07-16 16:18:22 +0200

    update

--- src/pages/Setting/index.tsx
+++ src/pages/Setting/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import style from './Setting.module.css'
+import Image from 'next/image';
+import SettingImg from '../../Asset/svg/Setting.svg';
 
 export default function Setting() {
-  return (
-    <div>Setting</div>
-  )
+	return (
+		<div className={style.SettingContent}>
+			<h1>Setting</h1>
+			
+			<Image src={SettingImg} alt='Setting'/>
+			
+		</div>
+	)
 }
