1b650bc829c7b25bdf61bb7604227a6e15810e0b
Hugo <hgranier20@gmail.com>
2023-07-17 15:41:31 +0200

    Add modal

--- src/pages/Planning/Room/[slug].tsx
+++ src/pages/Planning/Room/[slug].tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useRouter } from 'next/router';
 import Back from '../index';
 import Modal from '../../../../components/Modal/Modal';
@@ -7,27 +7,79 @@ import Image from 'next/image';
 import Input from '../../../../components/Input/Input';
 import Button from '../../../../components/Button/Button';
 import ImgModal from './../../../Asset/png/CardModal1.svg'
+import CardAlert from '../../../../components/CardAlert/CardAlert';
 
 export default function Plannig() {
     const router = useRouter();
     const { slug } = router.query;
+    const [Alerts, setAlerts] = useState([
+      {
+          Room: '001',
+          Type: 'Securite' ,
+          Resum: 'Name1',
+          id: 'id1'
+      },
+      {
+          Room: '001',
+          Type: 'Securite',
+          Resum: 'Name2',
+          id: 'id2'
+      },
+      {
+          Room: '001',
+          Type: 'Securite',
+          Resum: 'Name3',
+          id: 'id3'
+      },
+      {
+          Room: '001',
+          Type: 'Securite',
+          Resum: 'Name4',
+          id: 'id4'
+      },
+      {
+          Room: '001',
+          Type: 'Securite',
+          Resum: 'Name5',
+          id: 'id5'
+      },
+      {
+          Room: '001',
+          Type: 'Securite',
+          Resum: 'Name6',
+          id: 'id6'
+      }
+  ]);
 
     return (
         <>
             <Back />
             <Modal ToBack='/Planning'>
-                <h1>{slug}</h1>
+                {/* <h1>{slug}</h1> */}
                 <div className={styles.containerModal}>
-                  <div>gauche</div>
+                  <div className={styles.LeftSide}>
+                  <div className={styles.CardLeft}>
+                  <h2> Mes salles </h2>
+                    <div className={styles.ScrollContent}>
+                      {Alerts.map(AlertItem => (
+                            <CardAlert 
+                                key={AlertItem.id}
+                                Room={AlertItem.Room}
+                                Type={AlertItem.Type}
+                                Resum={AlertItem.Resum}
+                                id={AlertItem.id}
+                            />
+                        ))}</div>
+                    </div>
+                    <div className={styles.CardLeft2}>test</div></div>
                   <div className={styles.CardRight}>
-                  <h1 className={styles.Title}>S’inscrire</h1>
-          <Image className={styles.LoginImg} src={ImgModal} alt='CardLoginLeft' />
-          <div className={styles.InputDiv}>
-            <Input placeholder="Entrez votre nom" />
-            <Input placeholder="Entrez votre prénom" />
-            <Input placeholder="Entrez votre adresse email" />
-            <Button text="CONTINUE" />
-          </div>
+                   <h2 className={styles.Title}>Modification d'un événement</h2>
+                    <Image className={styles.LoginImg} src={ImgModal} alt='CardLoginLeft' />
+                    <div className={styles.InputDiv}>
+                      <Input placeholder="Nom de la salle " />
+                      <Input placeholder="Nombre de personne max" />
+                      <Button text="CONTINUE" />
+                    </div>
                   </div>
                 </div>
             </Modal>
