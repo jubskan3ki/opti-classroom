020f441e251a845de4b7f5c65cb808babc8cfbaa
Hugo <hgranier20@gmail.com>
2023-07-20 14:35:07 +0200

    Add button

--- src/pages/Planning/index.tsx
+++ src/pages/Planning/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Select from '../../../components/Select/Select'
 import Button from '../../../components/Button/Button';
 import styles from './Planning.module.css'
@@ -19,70 +19,186 @@ type PlanningProps = {
     initialWeeks: SelectOption[];
 };
 
-export default function Planning({ initialRooms, initialWeeks }: PlanningProps) {
 
-    const [selectedValue, setSelectedValue] = useState(initialRooms[0].id);
-   
+export default function Planning({ initialRooms, initialWeeks }: PlanningProps) {
+    
+
+    const test = 'id1';
+
+    const [selectedValue, setSelectedValue] = useState('');
+    const [modalOpen, setModalOpen] = useState(false);
+
+        const openModal = () => {
+            setModalOpen(true);
+        };
+
+        const closeModal = () => {
+            setModalOpen(false);
+        };
     const handleSelectChange = (id: string) => {
         setSelectedValue(id);
     };
+    const [selectsRoom, setSelectsRoom] = useState(initialRooms);
+    const [selectsWeek, setSelectsWeek] = useState(initialWeeks);
 
-    const selectedRoomLabel = initialRooms.find(room => room.id === selectedValue)?.label || 'Setting Room';
+        const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+        useEffect(() => {
+          const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+          };
+      
+          window.addEventListener('resize', handleResize);
+          return () => {
+            window.removeEventListener('resize', handleResize);
+          };
+        }, []);
+        const getContent = () => {
+            const [modalOpen, setModalOpen] = useState(false);
 
-    const days = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
-    const dates = ["17", "18", "19", "20", "21", "22", "23"];
+        const openModal = () => {
+            setModalOpen(true);
+        };
 
+        const closeModal = () => {
+            setModalOpen(false);
+        };
+
+            if (windowWidth >= 800) {
+              return <div className={styles.Container}>
+              <Title text='Planning'/>
+              <div className={styles.ContentHigh}>
+                  <Select 
+                  id="01" 
+                  options={selectsRoom} 
+                  selectedValue={selectedValue}
+                  onChange={handleSelectChange}    
+                  />
+                  <Select 
+                  id="01" 
+                  options={selectsWeek} 
+                  selectedValue={selectedValue}
+                  onChange={handleSelectChange}    
+                  />
+                  <div className={styles.ButtonLink}>
+                  <Link href={`/Planning/Room/${test}`}>
+                      <Button text='Setting Room' variant='primary' icon={faGear}></Button>
+                  </Link>
+                  </div>
+                  
+                  
+              </div>
+              <div className={styles.ContentPlanning}>
+                  <div className={styles.Content}>
+                  <div className={styles.ContentPlanningFistRow}>
+                      <CardPlanning textBlack="" textBlue="" />
+                  </div>
+                  <div className={styles.ContentHours}>
+                      <CardPlanning textBlack="Début :" textBlue="9h"/>
+                      <CardPlanning textBlack="Fin :" textBlue="12h30"/>
+                  </div>
+                  <div className={styles.ContentHours}>
+                      <CardPlanning textBlack="Début :" textBlue="13h30"/>
+                      <CardPlanning textBlack="Fin :" textBlue="17h"/>
+                  </div>
+                  
+                  </div>
+                  <div className={styles.Content}>
+                  <div className={styles.ContentPlanningFistRow}>
+                      <CardPlanning textBlack="Lundi" textBlue="17" />
+                  </div>
+                  <Link href={`/Planning/Event/${test}`}>
+                      <CardPlanningButton />
+                  </Link>
+                  
+                  <CardPlanningButton />
+                  </div>
+                  <div className={styles.Content}>
+                  <div className={styles.ContentPlanningFistRow}>
+                      <CardPlanning textBlack="Mardi" textBlue="18" />
+                  </div>
+                  <CardPlanningButton />
+                  <CardPlanningButton />
+                  </div>
+                  <div className={styles.Content}>
+                  <div className={styles.ContentPlanningFistRow}>
+                      <CardPlanning textBlack="Mercredi" textBlue="19" />
+                  </div>
+                  <CardPlanningButton />
+                  <CardPlanningButton />
+                  </div>
+                  <div className={styles.Content}>
+                  <div className={styles.ContentPlanningFistRow}>
+                      <CardPlanning textBlack="Jeudi" textBlue="20" />
+                  </div>
+                  <CardPlanningButton />
+                  <CardPlanningButton />
+                  </div>
+                  <div className={styles.Content}>
+                  <div className={styles.ContentPlanningFistRow}>
+                      <CardPlanning textBlack="Vendredi" textBlue="21" />
+                  </div>
+                  <CardPlanningButton />
+                  <CardPlanningButton />
+                  </div>
+                  <div className={styles.Content}>
+                  <div className={styles.ContentPlanningFistRow}>
+                      <CardPlanning textBlack="Samedi" textBlue="22" />
+                  </div>
+                  <CardPlanningButton />
+                  <CardPlanningButton />
+                  </div>
+                  <div className={styles.Content}>
+                  <div className={styles.ContentPlanningFistRow}>
+                      <CardPlanning textBlack="Dimanche" textBlue="23" />
+                  </div>
+                  <CardPlanningButton />
+                  <CardPlanningButton />
+                  </div>
+              </div>
+              </div>;
+            } else {
+              return <div>
+                <Title text='Plannig' />
+                <div className={styles.ContentHighMobile}>
+                <Select 
+                  id="01" 
+                  options={selectsRoom} 
+                  selectedValue={selectedValue}
+                  onChange={handleSelectChange}    
+                  />
+                  <Select 
+                  id="01" 
+                  options={selectsWeek} 
+                  selectedValue={selectedValue}
+                  onChange={handleSelectChange}    
+                  />
+                  <div className={styles.ButtonLink}>
+                  <Link href={`/Planning/Room/${test}`}>
+                      <Button text='Setting Room' variant='primary' icon={faGear}></Button>
+                  </Link>
+                </div>
+                </div>
+                <div>
+                    <div className={styles.ContainerLeftMobile}>
+                    <div className={styles.t}>
+                      <CardPlanning textBlack="" textBlue="" />
+                  </div>
+                  <div className={styles.t}>
+                      <CardPlanning textBlack="Début :" textBlue="9h"/>
+                      <CardPlanning textBlack="Fin :" textBlue="12h30"/>
+                  </div>
+                  <div className={styles.t}>
+                      <CardPlanning textBlack="Début :" textBlue="13h30"/>
+                      <CardPlanning textBlack="Fin :" textBlue="17h"/>
+                  </div> 
+                    </div>
+                </div>
+              </div>;
+            }
+          };
     return (
-        <div className={styles.Container}>
-        <Title text='Planning'/>
-        <div className={styles.ContentHigh}>
-            <Select 
-                id="01" 
-                options={initialRooms} 
-                selectedValue={selectedValue}
-                onChange={handleSelectChange}    
-            />
-            <Select 
-                id="02" 
-                options={initialWeeks} 
-                selectedValue={selectedValue}
-                onChange={handleSelectChange}    
-            />
-            <div className={styles.ButtonLink}>
-                <Link href={`/Planning/Room/${selectedValue}`}>
-                    <Button text={selectedRoomLabel} variant='primary' icon={faGear}></Button>
-                </Link>
-            </div>
-            
-            
-        </div>
-        <div className={styles.ContentPlanning}>
-            <div className={styles.Content}>
-                <div className={styles.ContentPlanningFistRow}>
-                    <CardPlanning textBlack="" textBlue="" />
-                </div>
-                <div className={styles.ContentHours}>
-                    <CardPlanning textBlack="Début :" textBlue="9h"/>
-                    <CardPlanning textBlack="Fin :" textBlue="12h30"/>
-                </div>
-                <div className={styles.ContentHours}>
-                    <CardPlanning textBlack="Début :" textBlue="13h30"/>
-                    <CardPlanning textBlack="Fin :" textBlue="17h"/>
-                </div>
-            </div>
-            
-            {days.map((day, index) => (
-                <div className={styles.Content} key={index}>
-                <div className={styles.ContentPlanningFistRow}>
-                    <CardPlanning textBlack={day} textBlue={dates[index]} />
-                </div>
-                <Link href={`/Planning/Event/${selectedValue}`}>
-                    <CardPlanningButton id={`btn${index+1}`} />
-                </Link>
-                    <CardPlanningButton id={`btn${index+2}`} />
-                </div>
-            ))}
-        </div>
+        <div>
+            {getContent()}
         </div>
     )
 }
