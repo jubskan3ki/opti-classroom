bb87f5c811652981af3201be418572723baf71c4
Hugo <hgranier20@gmail.com>
2023-07-20 10:43:11 +0200

    Add logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import styles from './Login.module.css';
 import Image from 'next/image';
 import CardLoginLeft from '../Asset/png/CardLoginLeft.svg';
 import CardLoginRight from '../Asset/png/CardLoginRight.svg';
+import Logo from '../Asset/png/Logo.svg'
 import Button from '../../components/Button/Button';
 import Input from '../../components/Input/Input';
 import Switch from '../../components/Switch/Switch';
@@ -26,6 +27,14 @@ export default function Home() {
   };
 
   return (
+    <div className={styles.AllContent}>
+      <div className={styles.LogoContent}>
+        <div className={styles.TextLogo}>
+          <div className={styles.Text}>Opti-ClassRoom</div> 
+          <Image className={styles.Logo} src={Logo} alt='logo' /></div>
+       
+      </div>
+    
     <div className={styles.Content}>
       <div className={styles.ContentLeft}>
         <div className={styles.CardLeft}>
@@ -55,6 +64,7 @@ export default function Home() {
           </div>
         </div>
       </div>
+    </div>
     </div>
   );
 }
